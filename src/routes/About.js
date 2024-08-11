82e6c8c26f2ce04e4dc0928f25280f1dd2343a67
frmad <frmad21@student.sdu.dk>
2024-08-11 18:20:49 +0200

    Fix text

--- src/routes/About.js
+++ src/routes/About.js
@@ -12,54 +12,81 @@ function About() {
             {/* About Me Section */}
             <div className="aboutMe" id="about">
                 <div className="headerAbout">
-                    <img src={migImage} alt="Picture of me" className="aboutImage"/>
+                    {/*<img src={migImage} alt="Picture of me" className="aboutImage"/>*/}
                     <h4>Freja Marie Madsen</h4>
-                    <p className="centered">
-                            Energetic, dedicated, and organized engineering student with experience in
-                            service, IT, and communication. Background in web editing and a passion for
-                            project management and web development. Skilled in independent work but also
-                            excels in collaborative efforts. Curious and committed to personal and
-                            professional development, as well as striving to enhance my skills.
+                    <p className="aboutMeText">
+                        Hello, I'm Freja Marie Madsen, a 22-year-old with a passion for technology,<br/>
+                        communication, and continuous learning. My journey began with an interest in business and
+                        IT,<br/>
+                        which led me to pursue a Bachelor's degree in Software Engineering from the University of
+                        Southern Denmark (SDU).<br/>
+                        Along the way, I’ve developed a strong foundation in software development, web editing, and
+                        problem-solving,<br/>
+                        combined with valuable experience in customer service and leadership.<br/><br/>
+
+                        My background in IT has given me a unique set of technical skills, including proficiency in
+                        languages like Java,<br/>
+                        PHP, and JavaScript, as well as experience with databases, Docker, and Kubernetes.<br/>
+                        I take pride in my ability to tackle complex problems with a structured and practical
+                        approach,<br/>
+                        always aiming to deliver high-quality results.<br/><br/>
+
+                        In addition to my technical expertise, I am a highly organized and dedicated individual.<br/>
+                        My experience in customer-facing roles has sharpened my communication skills,<br/>
+                        allowing me to work effectively both independently and as part of a team.<br/>
+                        I’m driven by a strong work ethic and a commitment to excellence in everything I do, whether
+                        it’s leading a project,<br/>
+                        learning a new technology, or providing top-notch service to clients.<br/><br/>
+
+                        Beyond my professional life, I have a keen interest in frontend web development, design, and
+                        personal growth.<br/><br/>
+
+                        I’m always eager to explore new areas, enhance my skills, and take on new challenges that push
+                        me to grow both personally and professionally.<br/>
+                        If you're looking for someone who brings energy, dedication, and a versatile skill set to the
+                        table, let's connect.<br/>
+                        I’m excited to see where my journey takes me next and how I can contribute to the success of
+                        your team.
                     </p>
                 </div>
 
-                    <p className="centered-orange"><span>Mail:</span> frejamadsen1408@gmail.com</p>
-                    <p className="centered-orange"><span>Tel:</span> 21854409</p>
+                <p className="centered-orange"><span>Mail:</span> frejamadsen1408@gmail.com</p>
+                <p className="centered-orange"><span>Tel:</span> 21854409</p>
 
-                    <hr className="break"/>
+                <hr className="break"/>
 
-                    {/* Technical Skills */}
-                    <h4 className="header">Technical Skills</h4>
-                    <div className="box">
-                        {renderSkill("HTML", "html", "90%")}
-                        {renderSkill("CSS", "css", "80%")}
-                        {renderSkill("Java", "css", "80%")}
-                        {renderSkill("JavaScript", "js", "65%")}
-                        {renderSkill("PHP", "php", "60%")}
-                        {renderSkill("React", "php", "60%")}
-                        {renderSkill("SQL", "php", "60%")}
-                        {renderSkill("Python", "python", "30%")}
-                        {renderSkill("MongoDB", "python", "30%")}
-                        {renderSkill("Docker", "docker", "20%")}
-                        {renderSkill("Kubernetes", "docker", "20%")}
-                        {renderSkill("CI/CD", "docker", "20%")}
-                    </div>
+                {/* Technical Skills */}
+                <h4 className="header">Technical Skills</h4>
+                <div className="box">
+                    {renderSkill("HTML", "html", "90%")}
+                    {renderSkill("CSS", "css", "80%")}
+                    {renderSkill("Java", "css", "80%")}
+                    {renderSkill("JavaScript", "js", "65%")}
+                    {renderSkill("PHP", "php", "60%")}
+                    {renderSkill("React", "php", "60%")}
+                    {renderSkill("SQL", "php", "60%")}
+                    {renderSkill("Python", "python", "30%")}
+                    {renderSkill("MongoDB", "python", "30%")}
+                    {renderSkill("Docker", "docker", "20%")}
+                    {renderSkill("Kubernetes", "docker", "20%")}
+                    {renderSkill("CI/CD", "docker", "20%")}
+                </div>
 
-                    {/* Semi-technical Skills */}
-                    <h4 className="header">Semi-technical Skills</h4>
-                    <div className="box">
-                        {renderSkill("Word", "html", "90%")}
-                        {renderSkill("Excel", "css", "80%")}
-                        {renderSkill("Powerpoint", "css", "80%")}
-                    </div>
+                {/* Semi-technical Skills */}
+                <h4 className="header">Semi-technical Skills</h4>
+                <div className="box">
+                    {renderSkill("Word", "html", "90%")}
+                    {renderSkill("Excel", "css", "80%")}
+                    {renderSkill("Powerpoint", "css", "80%")}
+                </div>
 
-                    {/* Language Skills */}
-                    <h4 className="header">Language Skills</h4>
-                    <div className="box">
-                        {renderSkill("Danish", "danish", "100%")}
-                        {renderSkill("English", "css", "80%")}
-                        {renderSkill("German", "php", "60%")}
-                    </div>
+                {/* Language Skills */}
+                <h4 className="header">Language Skills</h4>
+                <div className="box">
+                    {renderSkill("Danish", "danish", "100%")}
+                    {renderSkill("English", "css", "80%")}
+                    {renderSkill("German", "php", "60%")}
+                </div>
             </div>
         </div>
     );
@@ -68,7 +95,7 @@ function About() {
 // Helper function to render skill progress bars
 function renderSkill(name, className, level) {
     return (
-        <div>
+        <div className="skill">
             <p>{name}</p>
             <div className="container">
                 <div className={`skills ${className}`}>{level}</div>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -36,10 +36,8 @@ function Home() {
                         <p className="intro-text">
                             Hello and welcome! I'm a 22-year-old woman driven by energy, commitment, and a structured
                             approach to everything I do. With a diverse background in service, IT, and communication, I
-                            bring a wealth of experience in
-                            web editing and a passion for leadership and marketing. Curiosity drives me to constantly
-                            develop personally and
-                            professionally, always striving to enhance my skills and capabilities.
+                            bring a wealth of experience in web editing and a passion for leadership and marketing. Curiosity drives me to constantly
+                            develop personally and professionally, always striving to enhance my skills and capabilities.
                         </p>
 
                         <a href={resume} download="Freja-resume" className="resume-link">
@@ -62,23 +60,22 @@ function Home() {
                     </button>
                 </div>
                 <p>
-                    This is some additional content that appears below the wave. You can include more details about
-                    yourself or any other information that you want to highlight here.
+                    In this section, you'll find further insights into my professional journey, skills, and the unique experiences that shape my work.
                 </p>
                 <div className="flex-container">
                     <div>
-                        <h4>Text</h4>
-                        <p>Text</p>
+                        <h4>Creative Problem Solving</h4>
+                        <p>I thrive in environments where creative problem solving is key. My background in software engineering has instilled in me a passion for tackling complex technical challenges with innovative solutions. Whether it's designing a new feature, debugging a tricky issue, or optimizing performance, I enjoy the process of finding elegant and effective solutions to technical problems. The thrill of transforming an idea into a functional product or improving an existing system is what drives my enthusiasm for technology.</p>
                     </div>
 
                     <div>
-                        <h4>Text</h4>
-                        <p>Text</p>
+                        <h4>Collaborative Project Management</h4>
+                        <p>Collaboration and teamwork are at the heart of what I enjoy most in my work. I find immense satisfaction in working with diverse teams to achieve common goals. Whether leading a project or contributing as a team member, I value the exchange of ideas, the synergy of collective effort, and the opportunity to learn from others. My experience in customer service and project management has honed my ability to communicate effectively, coordinate tasks, and ensure that projects run smoothly and meet objectives.</p>
                     </div>
 
                     <div>
-                        <h4>Text</h4>
-                        <p>Text</p>
+                        <h4>Continuous Learning and Development</h4>
+                        <p>I am passionate about continuous learning and personal growth. In the fast-evolving fields of technology and design, staying updated with the latest trends and advancements is crucial. I love diving into new technologies, exploring emerging tools, and expanding my skill set. This drive for learning not only keeps me engaged and motivated but also enables me to bring fresh perspectives and innovative approaches to my work.</p>
                     </div>
                 </div>
             </div>
